d3486dcbdb1e853562ad32f9142ac1cd7ced2338
a487czz <leochp@gmail.com>
2017-04-06 13:47:41 -0300

    Heroku deploy - Static files hospedados no AWS S3

--- simplecrud/settings/base.py
+++ simplecrud/settings/base.py
@@ -119,13 +119,16 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
+
+print(STATIC_ROOT)
+print(STATICFILES_DIRS)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
