1a74e08e2fd60322be3e6e7c1bb4dd3dbd9c91ed
a487czz <leochp@gmail.com>
2017-04-06 16:00:16 -0300

    Fix heroku storage

--- simplecrud/urls.py
+++ simplecrud/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
 
 from django.views.generic import RedirectView
 
@@ -24,3 +25,11 @@ urlpatterns = [
     url(r'^produto/', include('oper.urls'), name='produto'),
     url(r'^accounts/', include('allauth.urls')),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
